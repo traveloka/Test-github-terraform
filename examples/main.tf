d4f4695061b221071a580001d96c81b1d3d4673f
Muhammad Fadhilah Sukmojatmiko <t-fadhilah@traveloka.com>
2021-10-13 17:33:50 +0700

    terraform fmt tf file lambda role in examples

--- examples/main.tf
+++ examples/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 module "this" {
   # In actual use case, you have to replace the following line (line 8) with:
-   source = "github.com/traveloka/terraform-aws-iam-role.git//modules/lambda?ref=v2.0.2"
- # source = "../../modules/lambda"
+  source = "github.com/traveloka/terraform-aws-iam-role.git//modules/lambda?ref=v2.0.2"
+  # source = "../../modules/lambda"
 
   product_domain   = "fdl"
   service_name     = "txtjobs"
