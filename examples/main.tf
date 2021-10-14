d851df082c02c3cdc09a90e89fd6709a5b90c0a1
Muhammad Fadhilah Sukmojatmiko <48316651+didilmfs@users.noreply.github.com>
2021-10-14 13:48:56 +0700

    create tf file to create dummy lambda role (#2)
    * create tf file to create dummy lambda role
    
    * terraform fmt tf file lambda role in examples

---
+++ examples/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "this" {
+  # In actual use case, you have to replace the following line (line 8) with:
+  source = "github.com/traveloka/terraform-aws-iam-role.git//modules/lambda?ref=v2.0.2"
+  # source = "../../modules/lambda"
+
+  product_domain   = "fdl"
+  service_name     = "txtjobs"
+  descriptive_name = "Periodic Scheduler"
+  environment      = "testing"
+}
