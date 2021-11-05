54884a9e3a2ec6c0449a7316ddd7eb08c7985062
Muhammad Fadhilah Sukmojatmiko <48316651+didilmfs@users.noreply.github.com>
2021-11-05 17:15:32 +0700

    Add backend for examples (#4)

--- examples/main.tf
+++ examples/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+terraform {
+  backend "s3" {
+    #    bucket         = "default-terraform-state-ap-southeast-1-460124681500"
+    bucket = "test-fadhil-bucket-lambda-220920211239"
+    key    = "examples/terraform.tfstate"
+    region = "ap-southeast-1"
+    #    dynamodb_table = "default-terraform-state-ap-southeast-1-460124681500"
+  }
+}
+
 module "this" {
   # In actual use case, you have to replace the following line (line 8) with:
   source = "github.com/traveloka/terraform-aws-iam-role.git//modules/lambda?ref=v2.0.2"
