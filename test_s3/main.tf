a02b2c9740646860f7cd9daa8649cd6adc6feb44
Muhammad Fadhilah Sukmojatmiko <t-fadhilah@traveloka.com>
2021-12-15 10:09:18 +0700

    Test create S3

---
+++ test_s3/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "test-fadhil-bucket-lambda-220920211239"
+    key            = "test_s3/terraform.tfstate"
+    region         = "ap-southeast-1"
+  }
+}
+
+
+resource "aws_s3_bucket" "test" {
+  bucket = "test-fadhil-151220210925"
+  acl    = "private"
+
+  tags = {
+    Name        = "test-fadhil-151220210925"
+    Environment = "testing"
+  }
+}
