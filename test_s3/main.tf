6888bd8d7d5af836c4971b76e628b912fac54a70
Muhammad Fadhilah Sukmojatmiko <t-fadhilah@traveloka.com>
2021-12-15 10:13:26 +0700

    remove backend

--- test_s3/main.tf
+++ test_s3/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "test-fadhil-bucket-lambda-220920211239"
-    key            = "test_s3/terraform.tfstate"
-    region         = "ap-southeast-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "test-fadhil-bucket-lambda-220920211239"
+#    key            = "test_s3/terraform.tfstate"
+#    region         = "ap-southeast-1"
+#  }
+#}
 
 
 resource "aws_s3_bucket" "test" {
